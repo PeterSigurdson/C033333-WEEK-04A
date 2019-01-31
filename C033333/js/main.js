77c146a75e1e37bcd7bf85c3bc6b2102f4f7835f
peter sigurdson <peter@petersigurdson.net>
2019-01-31 11:43:52 -0500

    I created a function and loaded it with the body onload event

--- C033333/js/main.js
+++ C033333/js/main.js
@@ -1,1 +1,7 @@
-﻿// Your code here!
+﻿var cat = new Date();
+
+function myFunction() {
+    
+    document.body.innerHTML = " <h1> The date is " + (cat.getMonth() + 1) + " / " + cat.getDate() + " / " + cat.getFullYear() + "  </h1>  ";
+}
+
